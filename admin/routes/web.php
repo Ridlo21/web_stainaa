1b328b3bebf0a248c4441f3c44dd174e6d228b03
oles <muklek99@gmail.com>
2024-11-28 23:56:05 +0700

    ok

--- admin/routes/web.php
+++ admin/routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Login;
+use App\Http\Controllers\Home;
+use App\Http\Middleware\UseLogin;
 
-Route::get('/', function () {
-    return view('dashboard.dashboard');
-});
 
-Route::get('/login', function () {
-    return view('login');
-});
+// Route::get('/', function () {
+//     return view('dashboard.dashboard');
+// });
+// Route::get('/home', [Home::class, 'index'])->middleware('auth');
+Route::get('/', [home::class, 'index'])->middleware('admin');
+Route::get('/login', [Login::class, 'index']);
+Route::post('/singin', [Login::class, 'singin'])->name('singin');
